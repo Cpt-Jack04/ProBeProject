3c7c535ff40b291ba6d94915737fe2189b5c114e
Alex Cline <clinealex10@gmail.com>
2019-03-13 10:51:24 -0500

    New Prototype Set Up

---
+++ ProBe Project V2/ProBe Project V2/Assets/Scripts/TextChatBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextChatBehavior : MonoBehaviour
+{
+    private Canvas textChatCanvas = null;
+    [SerializeField] private Image chatBoxImage = null;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void SpawnTextBubble(string textMessage)
+    {
+        Instantiate(chatBoxImage, Vector3.zero, Quaternion.identity, textChatCanvas.transform);
+    }
+}
